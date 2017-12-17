2d5397a1cb6de8f41d3ca64484244e4914f38982
David Franklin <dave5801@gmail.com>
2017-12-16 17:43:40 -0800

    hello world view

--- django_imager/imager_profile/views.py
+++ django_imager/imager_profile/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def home_view(request):
+    """Home view callable, for the home page."""
+    return HttpResponse("Hello World!")
