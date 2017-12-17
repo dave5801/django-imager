e6229989012e41a3a54dcc1d1431fd13d374e958
David Franklin <dave5801@gmail.com>
2017-12-16 16:49:56 -0800

    imager profile model created

--- django_imager/django_imager/settings.py
+++ django_imager/django_imager/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'imager_profile'
 ]
 
 MIDDLEWARE = [
@@ -86,6 +87,7 @@ DATABASES = {
         }
     }
 }
+
 
 
 # Password validation
