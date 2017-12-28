f82e7d94097c104423b01c7972f9ec439819767e
David Franklin <dave5801@gmail.com>
2017-12-28 10:41:17 -0800

    changed file path in register_view to register/...

--- django_imager/imager_profile/views.py
+++ django_imager/imager_profile/views.py
@@ -33,16 +33,14 @@ def register_view(request, page='Register'):
             user.save()
             current_site = get_current_site(request)
             subject = 'Activate Your MySite Account'
-            message = render_to_string('imager_profile/user_activation.html', {
+            message = render_to_string('registration/user_activation.html', {
                 'user': user,
                 'domain': current_site.domain,
                 'uid': urlsafe_base64_encode(force_bytes(user.pk)),
                 'token': account_activation_token.make_token(user),
             })
             user.email_user(subject, message)
-            return render(request, 'imager_profile/activation_sent.html')
+            return render(request, 'registration/activation_sent.html')
     else:
         form = EmailRegistrationForm()
-        #this line begins the error #returning bad response
-       # import pdb; pdb.set_trace()
-    return render(request, 'imager_profile/register.html', {'form': form, 'page': page})
+    return render(request, 'registration/register.html', {'form': form, 'page': page})
